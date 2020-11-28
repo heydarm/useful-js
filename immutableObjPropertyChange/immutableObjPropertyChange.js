c37084083cfb8a14b9fa6b224f491a486d0364ff
Heydar Muradov <56446558+heydarm@users.noreply.github.com>
2020-11-28 20:56:50 +0400

    immutableObjPropertyChange: Made some improvements

--- immutableObjPropertyChange/immutableObjPropertyChange.js
+++ immutableObjPropertyChange/immutableObjPropertyChange.js
@@ -1,11 +1,24 @@
 function immutableObjPropertyChange(obj, keysPath, val, i = 0) {
-  if (i < keysPath.length) {
-    return {
-      ...obj,
-      [keysPath[i]]:
-        typeof obj[keysPath[i]] === "object"
-          ? immutableObjPropertyChange(obj[keysPath[i]], keysPath, val, ++i)
-          : val,
-    };
+  if (!keysPath) {
+    throw new Error("'keys path' (second argument) is not defined");
   }
+
+  if (!Array.isArray(keysPath)) {
+    throw new Error("'keys path' (second argument) is not an array");
+  }
+
+  if (keysPath[i] === undefined || keysPath[i] === null) {
+    throw new Error(
+      "one of 'keys path' (second argument) elements is not defined"
+    );
+  }
+
+  console.log(i < keysPath.length);
+  return {
+    ...obj,
+    [keysPath[i]]:
+      i < keysPath.length - 1
+        ? immutableObjPropertyChange(obj[keysPath[i]], keysPath, val, ++i)
+        : val,
+  };
 }
